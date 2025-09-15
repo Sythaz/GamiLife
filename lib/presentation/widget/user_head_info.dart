2c59dc37d79cef43e29d3c5cf7510cc81c9e7dd7
Sythaz <syafiqaldiansyah@gmail.com>
2025-09-15 19:51:34 +0700

    feat: add name and banner section widegt

---
+++ lib/presentation/widget/user_head_info.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../core/constants/colors.dart';
+
+class UserHeadInfo extends StatelessWidget {
+  const UserHeadInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final maxWidth = constraints.maxWidth;
+
+        return SizedBox(
+          width: maxWidth,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hello, Syafiq Aldiansyah!',
+                    style: TextStyle(
+                      color: AppColors.white,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 12,
+                    ),
+                  ),
+                  Text(
+                    'The Legends',
+                    style: TextStyle(
+                      color: AppColors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              SvgPicture.asset('assets/images/badges/badge_7.svg', width: 45),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
