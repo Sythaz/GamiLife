c0d8d1a097773de09fbc2372de4960cdf9b4c6ce
Sythaz <syafiqaldiansyah@gmail.com>
2025-09-15 19:52:42 +0700

    feat: implement avatar, name section, level widget to HomePage

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -5,7 +5,10 @@ import 'package:gamilife/core/constants/colors.dart';
 import '../widget/banner_quest.dart';
 import '../widget/carousel_home.dart';
 import '../widget/heatmap_activities.dart';
+import '../widget/level_bar_container.dart';
 import '../widget/timeline_recent_activities.dart';
+import '../widget/user_avatar.dart';
+import '../widget/user_head_info.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -47,79 +50,85 @@ class _HomePageState extends State<HomePage> {
 
     return Scaffold(
       backgroundColor: AppColors.primary,
-      body: Stack(
-        children: [
-          const Center(
-            child: Text(
-              'Home Page',
-              style: TextStyle(
-                color: AppColors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ),
-          DraggableScrollableSheet(
-            initialChildSize: 0.7,
-            minChildSize: 0.7,
-            maxChildSize: 0.97,
-            builder: (context, scrollController) {
-              return Container(
-                decoration: const BoxDecoration(
-                  color: AppColors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(46),
-                    topRight: Radius.circular(46),
-                  ),
-                ),
-                child: ListView(
-                  padding: EdgeInsets.only(top: 6.5),
-                  controller: scrollController,
-                  children: [
-                    Center(
-                      child: Container(
-                        width: 55,
-                        height: 3.5,
-                        decoration: BoxDecoration(
-                          color: AppColors.gray2,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 6.5),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CarouselHome(
-                          controller: _controller,
-                          current: _current,
-                          onPageChanged: (index, reason) {
-                            setState(() {
-                              _current = index;
-                            });
-                          },
-                          items: [
-                            BannerQuest(),
-                            CustomHeatmapContribution(
-                              contributionData: sampleData,
+      body: SafeArea(
+        bottom: false,
+        child: Stack(
+          children: [
+            Positioned(
+              left: 16,
+              right: 16,
+              child: Column(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      UserAvatar(),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            UserHeadInfo(),
+                            SizedBox(height: 6),
+                            LevelBarContainer(
+                              level: 79,
+                              currentXP: 7681,
+                              maxXP: 9999,
                             ),
                           ],
                         ),
-                        const SizedBox(height: 16),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 16),
-                          child: Text(
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            DraggableScrollableSheet(
+              initialChildSize: 0.7,
+              minChildSize: 0.7,
+              maxChildSize: 0.95,
+              builder: (context, scrollController) {
+                return Container(
+                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                  decoration: const BoxDecoration(
+                    color: AppColors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(46),
+                      topRight: Radius.circular(46),
+                    ),
+                  ),
+                  child: ListView(
+                    // padding: EdgeInsets.only(top: 6.5),
+                    controller: scrollController,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CarouselHome(
+                            controller: _controller,
+                            current: _current,
+                            onPageChanged: (index, reason) {
+                              setState(() {
+                                _current = index;
+                              });
+                            },
+                            items: [
+                              BannerQuest(),
+                              CustomHeatmapContribution(
+                                contributionData: sampleData,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 16),
+                          Text(
                             'Recent Activities',
                             style: TextStyle(
                               fontSize: 18,
                               fontWeight: FontWeight.w600,
                             ),
                           ),
-                        ),
-                        const SizedBox(height: 10),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 16),
-                          child: Column(
+                          const SizedBox(height: 10),
+                          Column(
                             children: [
                               TimelineRecentActivities(
                                 time: '08:30',
@@ -152,16 +161,15 @@ class _HomePageState extends State<HomePage> {
                               ),
                             ],
                           ),
-                        ),
-                        const SizedBox(height: 120),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
-        ],
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
